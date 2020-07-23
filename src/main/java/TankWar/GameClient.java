1e9ef0ddd926d5d9aaebeb203b96fe41b7407903
silenthunter-chb <silenthunter.chb@gmail.com>
2020-07-24 01:09:54 +0800

    游戏主程序入口

--- src/main/java/TankWar/GameClient.java
+++ src/main/java/TankWar/GameClient.java
@@ -1,5 +1,26 @@
 package TankWar;
 
-public class GameClient {
+import javax.swing.*;
+import java.awt.*;
+
+public class GameClient extends JComponent {
+
+    public GameClient(){
+        this.setPreferredSize(new Dimension(800, 600));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.drawImage(new ImageIcon("assets/images/tankD.gif").getImage(), 400, 100, null);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Udemy的坦克大战");
+        frame.add(new GameClient());
+        frame.pack();
+        frame.setDefaultCloseOperation(3);
+        frame.setVisible(true);
+    }
 
 }

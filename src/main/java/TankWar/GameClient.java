a3cffa5fda8205b0406c01b0f420fc6a738e0f0f
silenthunter-chb <silenthunter.chb@gmail.com>
2020-07-24 22:57:48 +0800

    代码主目录

--- src/main/java/TankWar/GameClient.java
+++ src/main/java/TankWar/GameClient.java
@@ -2,6 +2,9 @@ package TankWar;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class GameClient extends JComponent {
 
@@ -11,12 +14,19 @@ public class GameClient extends JComponent {
 
     @Override
     protected void paintComponent(Graphics g) {
-        g.drawImage(new ImageIcon("assets/images/tankD.gif").getImage(), 400, 100, null);
+//        g.drawImage(new ImageIcon("assets/images/tankD.gif").getImage(), 400, 100, null);
+        try {
+            g.drawImage(ImageIO.read(new File("assets/images/tankD.gif")), 400, 100, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setTitle("Udemy的坦克大战");
+        frame.setIconImage(new ImageIcon("assets/images/icon.png").getImage());
+        frame.setLocation(300, 40);
         frame.add(new GameClient());
         frame.pack();
         frame.setDefaultCloseOperation(3);
